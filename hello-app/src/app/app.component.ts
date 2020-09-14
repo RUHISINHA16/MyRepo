897e385b82ad6f5cae5e59850e35968334d2fe26
RUHISINHA16 <rruhisinha16@gmail.com>
2020-09-14 13:10:47 +0530

    1st vs code commit

---
+++ hello-app/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <!--The content below is only a placeholder and can be replaced.-->
+    <div style="text-align:center" class="content">
+      <h1>
+        Welcome to {{title}}!
+      </h1>
+     <h2> My First Github An Jenkins Integration in vs code
+    
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'hello-app';
+}
